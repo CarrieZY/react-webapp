113226f9f6ae5eda01d891a437bc14f68b27dcdd
2275182981 <2275182981@qq.com>
2019-11-15 16:21:29 +0800

    脚手架搭建

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div>
+      <div><a>link</a></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navheader/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.scss'
+class Navheader extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div className="header">头部导航</div>
+        )
+    }
+}
+
+export default Navheader
